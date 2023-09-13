233452930790535ff8f1b865ad1581a96872f09a
ggbarrientos <143198944+ggbarrientos@users.noreply.github.com>
2023-09-13 14:23:02 +0000

    message

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ name = input("What is your name? ")
 sales = int(input("How many sales do you have? "))
 commissions = float(round((sales * 0.13),2))
 print(f"{name} made {sales} sales, and is entitled to {commissions} commissions.")
+####
